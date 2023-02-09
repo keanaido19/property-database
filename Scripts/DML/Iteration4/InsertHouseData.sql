ff9130e6029cee0b9f12692467ea004f1c5eb16a
Keaton <keanaido19@gmail.com>
2023-02-09 23:54:13 +0200

    Inserted mock House data

---
+++ Scripts/DML/Iteration4/InsertHouseData.sql
@@ -0,0 +1,33 @@
+USE PropertyManagementSystem
+
+EXEC uspInsertHouseData '7 Elk Place', 'Cape Town', 'Western Cape', '2039', 'South Africa'
+
+EXEC uspInsertHouseData '10 Queens Avenue', 'Durban', 'Kwa-Zulu Natal', '4392', 'South Africa'
+
+EXEC uspInsertHouseData '14 Smith Street', 'Johannesburg', 'Gauteng', '2123', 'South Africa'
+
+EXEC uspInsertHouseData '3 Eagle Road', 'Cape Town', 'Western Cape', '2345', 'South Africa'
+
+EXEC uspInsertHouseData '34 Sugar Drive', 'Durban', 'Kwa-Zulu Natal', '4568', 'South Africa'
+
+EXEC uspInsertHouseData '70 Pine Avenue', 'Johannesburg', 'Gauteng', '2126', 'South Africa'
+
+EXEC uspInsertHouseData '2 Left Road', 'Cape Town', 'Western Cape', '2757', 'South Africa'
+
+EXEC uspInsertHouseData '23 Right Place', 'Durban', 'Kwa-Zulu Natal', '4976', 'South Africa'
+
+EXEC uspInsertHouseData '45 Singapore Drive', 'Johannesburg', 'Gauteng', '2645', 'South Africa'
+
+EXEC uspInsertHouseData '56 Heart Cresent', 'Cape Town', 'Western Cape', '2353', 'South Africa'
+
+EXEC uspInsertHouseData '22 Flower Street', 'Durban', 'Kwa-Zulu Natal', '4453', 'South Africa'
+
+EXEC uspInsertHouseData '98 Taxi Place', 'Johannesburg', 'Gauteng', '2353', 'South Africa'
+
+EXEC uspInsertHouseData '35 Olive Road', 'Cape Town', 'Western Cape', '2456', 'South Africa'
+
+EXEC uspInsertHouseData '3 Morning Avenue', 'Durban', 'Kwa-Zulu Natal', '4535', 'South Africa'
+
+EXEC uspInsertHouseData '8 Star Place', 'Johannesburg', 'Gauteng', '2456', 'South Africa'
+
+GO
