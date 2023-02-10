eb85b52f8b7e86e5a52df531ba5c280fc9ba5c7b
Keaton <keanaido19@gmail.com>
2023-02-10 03:01:34 +0200

    Added mock data for the persons

---
+++ Scripts/DML/Iteration4/InsertLocationData.sql
@@ -0,0 +1,27 @@
+USE PropertyManagementSystem
+
+EXEC uspInsertLocationData '20 Apple Street', 'Johannesburg', 'Gauteng', '2965', 'South Africa'
+
+EXEC uspInsertLocationData '52 Acorn Place', 'Johannesburg', 'Gauteng', '2423', 'South Africa'
+
+EXEC uspInsertLocationData '11 Liger Drive', 'Durban', 'Kwa-Zulu Natal', '2756', 'South Africa'
+
+EXEC uspInsertLocationData '6 Spring Section', 'Durban', 'Kwa-Zulu Natal', '2435', 'South Africa'
+
+EXEC uspInsertLocationData '98 Boring View', 'Johannesburg', 'Gauteng', '2876', 'South Africa'
+
+EXEC uspInsertLocationData '123 Cunning Place', 'Johannesburg', 'Gauteng', '4078', 'South Africa'
+
+EXEC uspInsertLocationData '1 Liver Road', 'CapeTown', 'Western Cape', '4756', 'South Africa'
+
+EXEC uspInsertLocationData '5 Tear Place', 'CapeTown', 'Western Cape', '5365', 'South Africa'
+
+EXEC uspInsertLocationData '86 Munch Way', 'CapeTown', 'Western Cape', '9785', 'South Africa'
+
+EXEC uspInsertLocationData '32 Fire Street', 'Johannesburg', 'Gauteng', '4654', 'South Africa'
+
+EXEC uspInsertLocationData '7 Pear Avenue', 'Johannesburg', 'Gauteng', '2546', 'South Africa'
+
+EXEC uspInsertLocationData '34 August Cresent', 'Durban', 'Kwa-Zulu Natal', '2978', 'South Africa'
+
+GO
---
+++ Scripts/DML/Iteration4/InsertSimpleUserData.sql
@@ -0,0 +1,23 @@
+USE PropertyManagementSystem
+
+EXEC uspInsertPersonInformation 'Bat', 'Man', 'batman@yahoo.com', '27', '0843987433'
+
+EXEC uspInsertPersonInformation 'Susan', 'Strong', 'superstrong@gmail.com', '44', '0339874532'
+
+EXEC uspInsertPersonInformation 'Elton', 'John', 'IamElton@hotmail.com', '27', '0102458904'
+
+EXEC uspInsertPersonInformation 'Jeff', 'Hardy', 'skaterboii35@hotmail.com', '27', '0122936144'
+
+EXEC uspInsertPersonInformation 'Blue', 'Yellow', 'tom23@gmail.com', '32', '0234792549'
+
+EXEC uspInsertPersonInformation 'Olivia', 'Oil', 'baguettes@yahoo.com', '27', '0850840231'
+
+EXEC uspInsertPersonInformation 'Reed', 'Aboek', 'JokerIsCool@gmil.com', '27', '0792430122'
+
+EXEC uspInsertPersonInformation 'Candle', 'Man', 'superHotFire@hotmail.com', '27', '077077217'
+
+EXEC uspInsertPersonInformation 'Window', 'Ten', 'ele11enBad@gmail.com', '27', '0653987424'
+
+EXEC uspInsertPersonInformation 'Jeff', 'Again', 'runitbackturbo14@bbd.co.za', '27', '0732031402'
+
+GO
